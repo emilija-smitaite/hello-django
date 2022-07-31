a5df67820ed0cf4a44092897d58a7d795cbeb48a
emilija-smitaite <emilijasmitaite@yahoo.com>
2022-07-31 22:15:37 +0000

    Finish testing

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -4,6 +4,7 @@ from .forms import ItemForm
 # Create your tests here.
 
 class TestItemForm(TestCase):
+    """Tests form"""
 
     def test_item_name_is_required(self):
         form = ItemForm({"name": ""})

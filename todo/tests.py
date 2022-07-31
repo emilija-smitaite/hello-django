2081165838f4029743752a00d98b488590dacb62
emilija-smitaite <emilijasmitaite@yahoo.com>
2022-07-31 11:50:38 +0000

    Modify user input, Testing lessons

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestDjango(TestCase):
+    
+    def test_this_thing_works1(self):
+        self.assertAlmostEqual(1, 1)
+
+    def test_this_thing_works2(self):
+        self.assertAlmostEqual(1, 3)
+
+    def test_this_thing_works3(self):
+        self.assertAlmostEqual(1, )
+
+    def test_this_thing_works4(self):
+        self.assertAlmostEqual(1, 0)
